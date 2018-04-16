0a9c061fadd715bcc88ec3cf1c4d2638315721e8
gpottow <gpottow@umich.edu>
2018-04-16 14:23:50 -0400

    added search/plot by resturant name

--- final-project.py
+++ final-project.py
@@ -26,18 +26,24 @@ class resturant:
                  type=None,
                  location=None,
                  rating=None,
-                 source=None):
+                 source=None,
+                 price=None):
         self.name = name
         self.type = type
         self.location = location
         self.rating = rating
         self.source = source
+        self.price = price
 
     def read_from_yelp_dict(self, yelp_dict):
         self.name = yelp_dict['name']
         self.rating = yelp_dict['rating']
         self.location = yelp_dict['location']['city']
         self.source = 'yelp'
+        try:
+            self.price = len(yelp_dict['price'])
+        except:
+            self.price = 0
         try:
             self.type = yelp_dict['categories'][1]['title']
         except:
@@ -47,6 +53,10 @@ class resturant:
         self.name = google_dict['name']
         self.rating = google_dict['rating']
         self.source = 'google'
+        try:
+            self.price = google_dict['price_level']
+        except:
+            self.price = 0
 
     def read_from_cache_dict(self, cache_dict):
         self.name = cache_dict['name']
@@ -54,6 +64,7 @@ class resturant:
         self.rating = cache_dict['rating']
         self.location = cache_dict['location']
         self.source = cache_dict['source']
+        self.price = cache_dict['price']
 
     def write_to_cache_dict(self):
         cache_dict = {}
@@ -62,6 +73,7 @@ class resturant:
         cache_dict['rating'] = self.rating
         cache_dict['location'] = self.location
         cache_dict['source'] = self.source
+        cache_dict['price'] = self.price
         return cache_dict
 
     def __str__(self):
@@ -72,6 +84,7 @@ class resturant:
 
 
 #run at the beggining of the program to setup the DB
+#must be run at the beggining of the program
 def init_db(db_name):
     try:
         conn = sqlite3.connect(db_name)
@@ -111,6 +124,7 @@ def init_db(db_name):
                                 'Name' TEXT,
                                 'Type' TEXT,
                                 'Rating' FLOAT,
+                                'Price' INT,
                                 'Location' INTEGER,
                                 'Source' INTEGER) """
         cur.execute(create_resturants)
@@ -177,10 +191,10 @@ def insert_resturants_to_db(resturant_list):
             city_id = cur.execute(statement, values).fetchone()[0]
 
             statement = """ INSERT INTO resturants
-                            ("Name", "Type", "Rating", "Location", "Source")
-                            VALUES (?, ?, ?, ?, ?)"""
+                            ("Name", "Type", "Rating", "Location", "Rating", "Source")
+                            VALUES (?, ?, ?, ?, ?, ?)"""
             insertion = (resturant.name, resturant.type, resturant.rating,
-                         city_id, source_id)
+                         city_id, resturant.price, source_id)
 
             cur.execute(statement, insertion)
 
@@ -196,8 +210,29 @@ def get_resturants_from_yelp(city, food_type):
     params = {'categories': food_type.lower(), 'location': city, 'limit': '50'}
     resturant_search = requests.get(base_url, params=params, headers=header)
     resturant_search_results = json.loads(resturant_search.text)
-
     result_list = resturant_search_results['businesses']
+
+    result_obj_list = []
+    for result in result_list:
+        r = resturant()
+        r.read_from_yelp_dict(result)
+        for category in result['categories']:
+            if food_type in category.values():
+                r.type = food_type
+        result_obj_list.append(r)
+
+    return result_obj_list
+
+#gets specific resturant from yelp
+def get_specific_resturant_from_yelp(city, food_type, resturant_name):
+    base_url = 'https://api.yelp.com/v3/businesses/search'
+    header = headers = {'Authorization': 'bearer %s' % secrets.yelp_api_key}
+
+    params = {'categories': food_type.lower(), 'term': resturant_name, 'location': city, 'limit': '50'}
+    resturant_search = requests.get(base_url, params=params, headers=header)
+    resturant_search_results = json.loads(resturant_search.text)
+    result_list = resturant_search_results['businesses']
+
     result_obj_list = []
     for result in result_list:
         r = resturant()
@@ -210,6 +245,7 @@ def get_resturants_from_yelp(city, food_type):
     return result_obj_list
 
 
+#gets resturants from google places API
 def get_resturants_using_google_places(city, food_type):
     search_url = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
     search_text = city + "+" + food_type
@@ -233,6 +269,31 @@ def get_resturants_using_google_places(city, food_type):
 
     return result_obj_list
 
+#gets resturants from google places API
+def get_specific_resturant_using_google_places(city, food_type, resturant_name):
+    search_url = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
+    search_text = city + "+" + food_type + "+" + resturant_name
+    params = {
+        'query': search_text,
+        'type': 'resturant',
+        'key': secrets.google_places_key
+    }
+
+    search = requests.get(search_url, params)
+    result_format = json.loads(search.text)
+    search_results = result_format['results']
+
+    result_obj_list = []
+    for result in search_results:
+        r = resturant()
+        r.read_from_google_dict(result)
+        r.location = city
+        r.type = food_type
+        result_obj_list.append(r)
+
+    return result_obj_list
+
+
 
 #uses unofficial api
 def get_resturants_using_open_table(city, food_type):
@@ -243,7 +304,6 @@ def get_resturants_using_open_table(city, food_type):
     pass
 
 
-##todo: add open table to get new data
 def get_resturants_using_cache(city, food_type):
     resturant_list = []
     unique_id = city + "_" + food_type.lower()
@@ -276,6 +336,93 @@ def get_resturants_using_cache(city, food_type):
 
     return resturant_list
 
+def get_specific_resturant_using_cache(city, food_type, resturant_name):
+    resturant_list = []
+    unique_id = city + "_" + food_type.lower() + "_" + resturant_name
+
+    if unique_id in cache_dict:
+        print("Getting cached data... ")
+        dict_list = cache_dict[unique_id]
+        for dict in dict_list:
+            r = resturant()
+            r.read_from_cache_dict(dict)
+            resturant_list.append(r)
+
+    else:
+        print("Getting new data... ")
+        resturant_list = get_specific_resturant_from_yelp(city, food_type.lower(), resturant_name)
+        google_list = get_specific_resturant_using_google_places(city, food_type, resturant_name)
+        for g in google_list:
+            resturant_list.append(g)
+        #todo: add Opentable
+        resturant_dict_list = []
+        for r in resturant_list:
+            r_dict = r.write_to_cache_dict()
+            resturant_dict_list.append(r_dict)
+
+        cache_dict[unique_id] = resturant_dict_list
+        dumped_json_cache = json.dumps(cache_dict)
+        fw = open(cache_file_name, 'w')
+        fw.write(dumped_json_cache)
+        fw.close()
+
+
+    return resturant_list
+
+#gets ratings for a specific restruant
+#reutrns list where first item is yelp and second is google
+def get_specific_resturant_rating(city, food_type, resturant_name):
+    conn = sqlite3.connect(db_name)
+    cur = conn.cursor()
+
+    #initizlize city id to impossible value
+    city_id = -1
+
+    #statement to find city_id
+    try:
+        statement = """SELECT Id
+                       FROM cities
+                       WHERE Name = (?) """
+
+        values = (city, )
+        city_id = cur.execute(statement, values).fetchone()[0]
+
+    except:
+        city_id = -1
+
+    statement = """SELECT count(*)
+                   FROM resturants
+                   WHERE Location = (?)
+                   AND Name LIKE (?)
+                   AND Type = (?) """
+    values = (city_id, resturant_name, food_type)
+
+    if not cur.execute(statement, values).fetchone()[0]:
+        list = get_specific_resturant_using_cache(city, food_type, resturant_name)
+        insert_resturants_to_db(list)
+
+    statement = """SELECT Id
+                   FROM cities
+                   WHERE Name = (?) """
+
+    values = (city, )
+    city_id = cur.execute(statement, values).fetchone()[0]
+
+    ratings_statement = """SELECT ROUND(AVG(Rating),2)
+                        FROM resturants
+                        WHERE Location = (?)
+                        AND Name LIKE (?)
+                        GROUP BY Source
+                        ORDER BY Source"""
+
+    values = (city_id, resturant_name)
+    cur.execute(ratings_statement, values)
+
+    average_ratings = []
+    for row in cur:
+        average_ratings.append(row)
+
+    return average_ratings
 
 #get average raitngs
 #returns list where first item is yelp second is google
@@ -330,6 +477,30 @@ def get_average_ratings_by_type(city, food_type):
 
     return average_ratings
 
+
+def plot_specific_restruant_by_source(city, food_type, resturant_name):
+    ratings = get_specific_resturant_rating(city, food_type, resturant_name)
+
+    yelp_rating = ratings[0][0]
+    google_rating = ratings[1][0]
+    trace1 = go.Bar(
+             x = ["Yelp", "Google"],
+             y = [yelp_rating, google_rating],
+             name = "Average food Ratings for " + resturant_name + " food in " + city
+    )
+    data = [trace1]
+    layout = go.Layout(
+        title = "Average food ratings for " + resturant_name + " food in " + city,
+        yaxis = dict(
+            range=[2, 5]
+        ),
+        barmode='basic'
+    )
+    fig = go.Figure(data=data, layout=layout)
+    py.plot(fig, filename='grouped-bar')
+
+
+#plots average ratings from google and yelp using plotly
 def plot_average_ratings_by_type(city, food_type):
     ratings = get_average_ratings_by_type(city, food_type)
     yelp_rating = ratings[0][0]
@@ -350,7 +521,10 @@ def plot_average_ratings_by_type(city, food_type):
     fig = go.Figure(data=data, layout=layout)
     py.plot(fig, filename='grouped-bar')
 
-plot_average_ratings_by_type("Detroit", "italian")
+init_db(db_name)
+
+plot_specific_restruant_by_source("Ann Arbor", "mexican", "Chipotle Mexican Grill")
+
 
 
 #here for spacing
